3d77190f4bdae5c3c2084f1cf3616bbf70ee03c6
may811204 <ju40268@gmail.com>
2016-06-23 02:58:25 +0800

    Delete showResidualImg.m

--- showResidualImg.m
+++
@@ -1,9 +0,0 @@
-function showResidualImg(img_ref,img8,img17,blockSize,searchLimit,mode)
-[PSNR1, SAD1,imgRC] = motion_estimate(img8, img_ref, blockSize,searchLimit,mode);
-figure();
-imshow(abs(imgRC-img8));
-[PSNR1, SAD1,imgRC] = motion_estimate(img17, img_ref, blockSize,searchLimit,mode);
-figure();
-imshow(abs(imgRC-img17));
-
-end
